654e72251a99f3415b9b4f51cb6e59ea79b866fc
Mauro Patricio <mpatricio@cedsif.gov.mz>
2026-02-17 14:06:16 +0200

    Actualizacao

--- admin-web/src/pages/SystemStatus.jsx
+++ admin-web/src/pages/SystemStatus.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
-import api from '../services/api';
+import api from '../api';
 
 const SystemStatus = () => {
     const [status, setStatus] = useState(null);
